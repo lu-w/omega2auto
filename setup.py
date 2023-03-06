45a0acf3de0954b454fe4386c074f8ebc7873f92
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-03-06 15:17:55 +0100

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="omega2auto",
+    version="0.1",
+    description="Python module for converting the OMEGA format into A.U.T.O. ABoxes",
+    author ="Lukas Westhofen",
+    author_email="lukas.westhofen@dlr.de",
+    py_modules=["omega2auto"],
+    install_requires=[
+        "pyauto",
+        "omega_format",
+        "shapely"
+    ]
+)
