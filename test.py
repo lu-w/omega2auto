1888412589785398014b4395bb4d11e0b7837dfd
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-10-20 11:03:24 +0200

    Removes test file

--- test.py
+++ test.py
@@ -7,12 +7,11 @@ from pyauto.models.scenario import Scenario
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s: %(message)s")
 
-scenarios = omega2auto.convert(sys.argv[1], hertz=5, start_offset=10, end_offset=15, folder="/home/lwesthofen/Dokumente/VVMethoden/SCMs/pyauto/src/pyauto/auto")
-scenarios[0].save_abox("/tmp/fka/00/scenario.owl")
+scenarios = omega2auto.convert(sys.argv[1], hertz=5, start_offset=20, end_offset=20.3, folder="/home/lwesthofen/Dokumente/VVMethoden/SCMs/pyauto/src/pyauto/auto")
+scenarios[0].save_abox("/tmp/fka/01/scenario.owl")
 visualizer.visualize(scenarios[0])
 #loaded = Scenario(folder="/home/lwesthofen/Dokumente/VVMethoden/SCMs/pyauto/src/pyauto/auto", file="/tmp/fka/00/scenario.kbs", hertz=5)
 #visualizer.visualize(loaded)
 
 # TODO successor lanes?
-# TODO Parking vehicle drivers
 # TODO visualization ped. crossing
